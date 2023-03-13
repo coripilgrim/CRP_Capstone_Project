d86370faee4fdfb98c462af5bdbbd41bbc15bba4
Cori Lynn Stevens (Pilgrim) <72805614+lynnpilgrim@users.noreply.github.com>
2023-03-12 21:37:14 -0400

    Add files via upload
    Objectives achieved: 1, 2, 3, 4, 5, 7, and 8.

--- project.py
+++ project.py
@@ -15,27 +15,37 @@ def select_folder2():
     folder_path = filedialog.askdirectory()
     print("Selected folder:", folder_path)
 
+def crp():
+    print("Your copies have been made.")
+    close_button = tk.Button(window, text="Close",command=window.destroy)
+    close_button.pack()
+
 #Create a button to browse for first folder.
 browse_button = tk.Button(window, text="Select old folder",command=select_folder1)
-browse_button.pack(padx=20, pady=20)
+browse_button.pack(padx=10, pady=10)
 
 #Create a button to browse for second folder.
 browse_button = tk.Button(window, text="Select new folder",command=select_folder2)
-browse_button.pack(padx=20, pady=20)
+browse_button.pack(padx=10, pady=10)
 
 #Add a widget for old text entry.
-label = tk.Label(text="Enter old text to remove.")
+label = tk.Label(text="Enter text to remove from filename.")
 label.pack()
-old_text = tk.Entry(window)
-label = tk.Label(text="Enter old text to remove.")
+old_text = tk.Entry()
+old_text.pack()
+label = tk.Label(text="Enter text to add to filename.")
 label.pack()
-new_text = tk.Entry(window)
+new_text = tk.Entry()
+new_text.pack()
 
 #Add a button to initiate copy and paste.
 big_green_button = tk.Button(
     text="Copy, rename, and paste files.",
-    bg="red")
-#big_green_button = crp
+    bg="green",
+    fg="white",
+    command=crp
+    )
+big_green_button.pack(padx=10, pady=10)
 
 #Main loop
 window.mainloop()
